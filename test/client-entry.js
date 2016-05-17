8c9925b84d5dec7e3994ab9c37f087e62072acbc
Kean Loong Tan <keanification@gmail.com>
2016-05-17 00:14:49 -0700

    Fix linting issue

--- test/client-entry.js
+++ test/client-entry.js
@@ -2,5 +2,5 @@ require("babel-polyfill");
 
 var srcReq = require.context("src", true, /\.jsx?$/);
 srcReq.keys().map(srcReq);
-var context = require.context('test', true, /.+\.spec\.jsx?$/);
+var context = require.context("test", true, /.+\.spec\.jsx?$/);
 context.keys().forEach(context);
